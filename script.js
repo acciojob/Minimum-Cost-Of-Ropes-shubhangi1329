069f330743878774f2167692fa3951c4eeb9ee89
shubhangi1329 <117506539+shubhangi1329@users.noreply.github.com>
2023-02-16 15:08:42 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 //your code here
+ function RopeCost(event){
+	 event.preventDefault();
+	 var input=document.querySelector('input');
+	 var arr=input.split(",");
+	 var cost=0;
+
+	 while(arr.length>1){
+		 var res=Number(arr[0])+Number(arr[1]);
+		 arr.push(res);
+		 cost+=res;
+		 arr.shift();
+		 arr.shift();
+		 arr.sort(a,b)=>{return a-b};
+	 }
+	 document.getElementById('result').textContent=cost;
+ }
